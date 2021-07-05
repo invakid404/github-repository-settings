dead6b6f1bb5e4e3f88acf79048ba2008ee132d5
invakid404 <invakid404@riseup.net>
2021-07-05 16:18:52 +0300

    feat: get rid of template code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,7 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+const run = async (): Promise<void> => {
+  core.info('Hello, world!');
+};
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
-
-run()
+run();
